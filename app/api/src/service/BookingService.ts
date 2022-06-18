9d9329927ecbabfc48e1e9a4303b5c904515dc40
Daniel <57826073+khoodan@users.noreply.github.com>
2022-06-18 18:32:13 +1000

    updated BookingService for test site get

--- app/api/src/service/BookingService.ts
+++ app/api/src/service/BookingService.ts
@@ -3,6 +3,7 @@ import { DynamoBookingClient } from "src/client/aws/DynamoBookingClient";
 import { BookingClient } from "src/client/BookingClient";
 import { DataService } from "./DataService";
 import { GetTestParams, testService } from "./TestService";
+import { GetTestSiteParams, testSiteService } from "./TestSiteService";
 import { GetUserParams, userService } from "./UserService";
 
 export interface GetBookingParams {
@@ -26,6 +27,10 @@ export class BookingService extends DataService {
     includeTestsTaken: true
   }
 
+  private static testSiteParams: GetTestSiteParams = {
+
+  }
+
   async getBookings({
     includeUser = true,
     includeTestSite = true,
@@ -35,6 +40,7 @@ export class BookingService extends DataService {
     const promises: Promise<any>[] = []
     if (includeUser) promises.push(this.getDataForItem(bookings, this.getUserForBooking))
     if (includeCovidTests) promises.push(this.getDataForItem(bookings, this.getTestsForBooking))
+    if (includeTestSite) promises.push(this.getDataForItem(bookings, this.getTestSiteForBooking))
     await Promise.all(promises)
     return bookings
   }
@@ -45,17 +51,11 @@ export class BookingService extends DataService {
     includeCovidTests = true
   }: GetBookingParams): Promise<BookingSchema> {
     const booking: AllBookingSchema = await this.bookingClient.getBookingForId(bookingId);
-
-    if (includeUser) {}
-
-    if (includeCovidTests) {
-      await testService.getTestsForIdList(booking.covidTestIds, {
-        includePatient: includeUser,
-        includeAdministerer: includeUser,
-        includeBooking: false
-      })
-    }
-
+    const promises: Promise<any>[] = []
+    if (includeUser) promises.push(this.getUserForBooking(booking))
+    if (includeCovidTests) promises.push(this.getTestsForBooking(booking))
+    if (includeTestSite) promises.push(this.getTestSiteForBooking(booking))
+    await Promise.all(promises)
     return booking
   }
 
@@ -69,6 +69,10 @@ export class BookingService extends DataService {
 
   private async getUserForBooking(booking: AllBookingSchema): Promise<void> {
     booking.customer = await userService.getUserById(booking.customerId, BookingService.userParams)
+  }
+
+  private async getTestSiteForBooking(booking: AllBookingSchema): Promise<void> {
+    booking.testingSite = await testSiteService.getTestSiteForId(booking.testingSiteId, BookingService.testSiteParams)
   }
 }
 
--- app/api/src/service/TestSiteService.ts
+++ app/api/src/service/TestSiteService.ts
@@ -1,7 +1,18 @@
+import { TestSiteSchema } from "@schema/LocationSchema";
+import { DynamoTestSiteClient } from "src/client/aws/DynamoTestSiteClient";
+import { TestSiteClient } from "src/client/TestSiteClient";
 import { DataService } from "./DataService";
 
-export class TestSiteService extends DataService {
+export interface GetTestSiteParams {
 
+}
+
+export class TestSiteService extends DataService {
+  private testSiteClient: TestSiteClient = new DynamoTestSiteClient()
+
+  getTestSiteForId(id: string, params: GetTestSiteParams): Promise<TestSiteSchema> {
+    return Promise.resolve({})
+  }
 }
 
 export const testSiteService = new TestSiteService()
